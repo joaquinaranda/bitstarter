9a5ea4681342e0801c041b70e7e88b3d05ae7a4d
Joaquin Aranda <joaquin.aranda.aragones@gmail.com>
2013-07-07 18:43:43 +0200

    updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var index = "index.html";
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!!!');
+
+  var output = new Buffer(16);
+  output = fs.readFileSync(index, 'utf-8');
+  
+  response.send(output.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
